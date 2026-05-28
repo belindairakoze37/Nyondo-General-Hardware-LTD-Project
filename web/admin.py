668dff65c1841edcf03da55256b7376975811c80
belindairakoze37 <belindairakoze37@gmail.com>
2026-05-28 10:16:04 +0300

    modified report dashboard

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . models import *
 
 # Register your models here.
+
+admin.site.register(Supplier)
+admin.site.register(Stock)
+admin.site.register(Sale)
+admin.site.register(Customer)
+admin.site.register(SupplierCredit)
+admin.site.register(Deposit)
+admin.site.register(Payment)
+
+
